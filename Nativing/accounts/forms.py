6a0d9e93904f75dbe91bb8e5b73e3592f4d2b244
JeongH0 <alswjdgh5095@gmail.com>
2021-08-01 20:02:08 +0900

    feat: make forms.py

---
+++ Nativing/accounts/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import *
+from django.contrib.auth.forms import UserCreationForm
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["username", "email", "nickname", "date_of_birth", "user_image", ]
+
+
+class LoginForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["email", "password"]
+        widgets = {"password": forms.PasswordInput}
