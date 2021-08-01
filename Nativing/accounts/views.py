ce3c6a1694e2e8f4eaa323f5883233a2901ea805
JeongH0 <alswjdgh5095@gmail.com>
2021-08-01 20:12:15 +0900

    feat: views.py basic setting

--- Nativing/accounts/views.py
+++ Nativing/accounts/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import *
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def accounts_signup(request):
+    return 
+
+
+def accounts_login(request):
+    return 
+
+
+def accounts_logout(request):
+    return 
+
+
+def accounts_home(request):
+    return
+
+
+def signup_success(request):
+    return
+
+
+def login_success(request):
+    return
+
+
+def logout_success(request):
+    return
