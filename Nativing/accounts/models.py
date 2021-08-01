4e080a6147b844b82335b29f6931045a772df181
JeongH0 <alswjdgh5095@gmail.com>
2021-08-01 20:01:49 +0900

    refactor: modify models.py

--- Nativing/accounts/models.py
+++ Nativing/accounts/models.py
@@ -2,6 +2,21 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 
 
+class Gender:
+    """
+    Gender
+    - Distinguish Male, Female
+    """
+    MALE = 'male'
+    FEMALE = 'female'
+
+    GENDER_TYPES = [
+        (MALE, '남성'),
+        (FEMALE, '여성'),
+    ]
+
+
+
 class User(AbstractBaseUser):
     username = models.CharField(max_length=30, null=True, blank=True)
     email = models.EmailField(
@@ -14,7 +29,7 @@ class User(AbstractBaseUser):
     withdrew_at = models.DateTimeField(blank=True, null=True, verbose_name='탈퇴 시점')
     date_of_birth = models.CharField(max_length=32, blank=True, help_text='생년월일')
     user_image = models.ImageField(upload_to='images/', blank=True, null=True)
-    user_gender = models.BooleanField(default=False)
+    user_gender = models.CharField(max_length=10, choices=Gender.GENDER_TYPES)
     user_age = models.PositiveIntegerField(blank=True, null=True)
     is_login = models.BooleanField(default=False)
 
