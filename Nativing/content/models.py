1664c356136bd334cf55c7c45ff156eaf5c5a75a
LeoBang <bluke8489@gmail.com>
2021-08-04 15:24:19 +0900

    Feat: initial view of explore

--- Nativing/content/models.py
+++ Nativing/content/models.py
@@ -21,7 +21,6 @@ class ContentUpload(models.Model):
     datetime = models.DateTimeField(default = timezone.now)
     image = models.ImageField(upload_to='images/', blank=True, null=True)
     tag = TaggableManager()
-
     #핵심표현 설명 선택지
     expression_descript_select = models.CharField(max_length=20, choices=EXPRESSION_CHOICES, default='ABBREVIATION')
     #상대와의 관계 선택지
--- Nativing/content/static/js/filter.js
+++ Nativing/content/static/js/filter.js
@@ -1,19 +1,41 @@
 const relationshipTag = document.getElementsByClassName("relationship-check")
 const searchResults = document.getElementsByClassName("content-box");
-const keyword = document.getElementById("just-for-keyword").innerText;
+let keyword = null;
+try {
+    keyword = document.getElementById("just-for-keyword").innerText;
+} catch (e) {
+    console.log(e);
+};
 const filterProperty = {
     keyword : [],
     relation : [],
     tag : []
 }
 
-
-if (keyword){
-    filterProperty.keyword.push(keyword);
-    console.log("키워드는 : ", keyword );
-} else {
-    filterProperty.keyword = [];
+// 검색어가 있으면, 검색어대로 filter + 검색어 filterProperty에 추가
+// 없으면 모든 모델 가져오고, filterProperty.keyword 초기화
+function initialView(){
+    fetch('/explore-filter',)
+        .then((res) => res.json())
+        .then((res) => {
+            if (keyword)  {
+                console.log("키워드는 :", keyword);
+                console.log("됐음");
+                filterProperty.keyword.push(keyword);
+                let filtered_content = [];
+                filtered_content = res.filter((value) => value.title.includes(keyword))
+                console.log(filtered_content);
+                printContent(filtered_content);
+  
+            } else {
+                console.log("안됐음");
+                filterProperty.keyword = [];
+                printContent(res);
+            }
+        })
 }
+
+
 
 function fetchContent() {
     fetch('/explore-filter',)
@@ -21,13 +43,23 @@ function fetchContent() {
         .then((res) => {
             console.log(res);
             let filtered_content = [];
-            for (let relation of filterProperty.relation){
+            for (let relationIter of filterProperty.relation){
                 // console.log(relation);
-                let filteredArray = res.filter((value) => value.relation_select === relation);
-                for (arrayComponent of filteredArray) {
+                // filter() 가 Array로 return 하므로 다시 한번 for문으로 추가해줌,,
+                let filteredArray = res.filter((value) => value.relation_select === relationIter);
+                for (let arrayComponent of filteredArray) {
                     filtered_content.push(arrayComponent);
                 }
             }
+            for (let keywordIter of filterProperty.keyword) {
+                let filteredArray = res.filter((value) => value.title.includes(keywordIter))
+                for (let arrayComponent of filteredArray) {
+                    if (!filtered_content.includes(arrayComponent)){
+                        filtered_content.push(arrayComponent);
+                    }
+                }
+            }
+
             console.log("체크된 컨텐츠", filtered_content)
             clearChildNode();
             printContent(filtered_content)
@@ -49,7 +81,8 @@ function printContent(value) {
 
         contentTitle.innerHTML = value[i].title;
         contentRelation.innerHTML = value[i].relation_select;
-        contentBody.innerHTML = i, "번째 글"
+        contentBody.innerHTML = 
+            `${value[i].expression} <span>is ${value[i].expression_descript_select}</span> of ${value[i].expression_descript}`
 
         contentBox.append(contentTitle, contentRelation, contentBody);
         
@@ -101,6 +134,7 @@ function checkEventRelation() {
 }
 
 function init(){
+    initialView();
     checkEventRelation();
     // fetchContent();
 }
