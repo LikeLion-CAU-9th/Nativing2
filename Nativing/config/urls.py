bd23f0b754ba47e2c1cb972c35d271825d4961af
k-mover <armoire@gimtaeyeong-ui-MacBookPro.local>
2021-08-05 22:56:37 +0900

    feat :Add project url path

--- Nativing/config/urls.py
+++ Nativing/config/urls.py
@@ -4,9 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('', include('content.urls')),
-    path('', include('upload.urls')),
+    path('content/', include('content.urls')),
+    path('upload/', include('upload.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
