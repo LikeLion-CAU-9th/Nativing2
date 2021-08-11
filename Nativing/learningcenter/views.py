cba2e50c190d8f46174413247f319a3cfc01aa53
LeoBang <bluke8489@gmail.com>
2021-08-12 03:08:52 +0900

    Feat: learning center following contents backend

--- Nativing/learningcenter/views.py
+++ Nativing/learningcenter/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from content.models import ContentUpload
+from accounts.models import User, Follow
 
 # Create your views here.
 
 def learning_center(request):
-
+    contents = Follow.objects.filter(follower_id = request.user.id).select_related('follower')
+    following_ids = []
+    for followee_iter in contents:
+        following_ids.append(followee_iter.followee.id)
     
-    return render(request, 'learning.html')
+    following_contents = []
+    for followee_id in following_ids:
+        content_temp = ContentUpload.objects.filter(writer_id = followee_id).select_related('writer')
+        following_contents.append(content_temp)
+    
+    return render(request,'learning.html', {"contents" : following_contents})
